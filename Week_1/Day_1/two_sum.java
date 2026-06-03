7c74d76fee61f4c4c66dd28d51d797f96b428b02
KumariAnjali2004 <anjaliyadav5may2004@gmail.com>
2026-06-03 14:09:27 +0530

    Update and rename test.java to two_sum.java

---
+++ Week_1/Day_1/two_sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] arr, int x) {
+      for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]==x){
+                return new int[]{i,j};
+            }          
+             }
+    }return new int[]{};
+}
+}
